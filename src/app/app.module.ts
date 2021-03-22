26b5d7b62e2b738bd9623539822304b3093f714b
Mario Flores <floresmario96@hotmail.com>
2021-03-22 13:59:15 -0600

    Estructura del proyecto lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BusquedaGeneralComponent } from './pages/busqueda-general/busqueda-general.component';
+import { TargetComponent } from './components/target/target.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BusquedaGeneralComponent,
+    TargetComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/busqueda-general/busqueda-general.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda-general',
+  templateUrl: './busqueda-general.component.html',
+  styleUrls: ['./busqueda-general.component.css']
+})
+export class BusquedaGeneralComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
