7f74104b66b69b4cdf0f760421c19851685a86be
Luis Angel <angel_apineda@hotmail.com>
2021-05-03 19:06:24 -0500

    cambios home

--- src/app/components/last-articles/last-articles.component.ts
+++ src/app/components/last-articles/last-articles.component.ts
@@ -17,43 +17,15 @@ export class LastArticlesComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.articleService.getLastArticles().subscribe( (articles: string) => {
-      // console.log(articles);
-      let articlesArray: string[] = articles.split('<<<');
+    this.articleService.getLastArticles().subscribe((articles: any) => {
+      console.log(articles.resultados);
+      this.articles = articles.resultados;
       // console.log(articlesArray);
-      articlesArray.forEach((article) => {
-        let dataArticle: string[] = article.split('%%%');
-        // console.log(dataArticle);
-        const cveArticle: number = Number( this.parserDataArticle(dataArticle[0]));
-        const cveRevista:number = Number( this.parserDataArticle(dataArticle[1]) );
-        const volumen:number = Number( this.parserDataArticle(dataArticle[2]) );
-        const numrevnum: string = this.parserDataArticle(dataArticle[3]);
-        const anio:number = Number( this.parserDataArticle(dataArticle[4]) );
-        const numero:number = Number( this.parserDataArticle(dataArticle[5]) );
-        const titulo:string = this.parserDataArticle(dataArticle[6]);
-        const urlRevista:string = this.parserDataArticle(dataArticle[7]);
-        const nombreRevista:string = this.parserDataArticle(dataArticle[8]);
-        const autores:string = this.parserDataArticle(dataArticle[9]);
-        const newArticle: Article = {
-          cveArticulo: cveArticle,
-          cveRevista,
-          volumen,
-          volRevNum: numrevnum,
-          anio,
-          numero,
-          tituloArticulo: titulo,
-          dominio: urlRevista,
-          nombreRevista,
-          autores
-        }
-        // console.log(newArticle);
-        this.articles.push(newArticle);
-      });
-      console.log(this.articles);
+      // console.log(this.articlesArray);
     });
   }
 
-  parserDataArticle(dataArticle: string){
+  parserDataArticle(dataArticle: string) {
     const data = dataArticle.split('::');
     return data[1];
   }
